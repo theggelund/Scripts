8ce9bad585128e004be2aa78a0d3210deb8ab2db
Thomas Heggelund <the@dips.no>
2019-02-04 12:50:28 +0100

    Adding assembly binding redirect generation script

---
+++ getAssemblyBindingRedirect.ps1
@@ -0,0 +1,19 @@
+﻿$files = Get-ChildItem *.dll
+
+function getAssemblyBindingRedirect([string] $target)
+{
+    $fullname = [System.Reflection.AssemblyName]::GetAssemblyName($target).FullName
+
+    $parts = $fullname -split ',' | % { $_.Trim() } | % { $_ -replace 'PublicKeyToken=','' } | % { $_ -replace 'Culture=', '' } | % { $_ -replace 'Version=','' }
+
+    return @"
+<dependentAssembly>
+    <assemblyIdentity name="$($parts[0])" publicKeyToken="$($parts[3])" culture="$($parts[2])" />
+    <bindingRedirect oldVersion="0.0.0.0-65535.65535.65535.65535" newVersion="$($parts[1])" />
+</dependentAssembly>
+"@
+}
+
+$content = $files | Sort-Object | % { getAssemblyBindingRedirect($_) } 
+
+$content -join [System.Environment]::NewLine
